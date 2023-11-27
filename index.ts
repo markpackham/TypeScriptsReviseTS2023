6cb4b0e792e930e6bc82cff198a77765c5d2cbbc
mark <markpackham1@gmail.com>
2023-11-27 14:13:02 +0000

    finish off basic types demo
    https://youtu.be/FGYwsnItyDs?feature=shared&t=3

--- index.ts
+++ index.ts
@@ -8,4 +8,17 @@ isFictional = true
 
 let planet = 'Earth'
 let moons = 1;
-let isLarge = false 
+let isLarge = false
+
+planet = 'Saturn'
+moons = 145
+isLarge = true
+
+// null & undefined
+let something: null
+let anotherThing: undefined
+
+something = null
+// this does seem pointless since undefined usually occurs when something has gone wrong
+// you typically don't want to ever assign anything undefined
+anotherThing = undefined
