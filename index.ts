c01751a58fa0c7831e368f46b5a83347da410132
mark <markpackham1@gmail.com>
2023-11-27 14:03:44 +0000

    demo basic types
    https://youtu.be/WcRgpER7i_g?feature=shared&t=184

--- index.ts
+++ index.ts
@@ -1,2 +1,11 @@
-let age = 30
-console.log(age)
+let age: number = 30
+let firstName: string = 'Mario'
+let isFictional: boolean
+
+age = 31
+firstName = 'Luigi'
+isFictional = true
+
+let planet = 'Earth'
+let moons = 1;
+let isLarge = false 
