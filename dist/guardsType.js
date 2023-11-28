1847ff48a4ea78bd6224adfdcc0254615f4753f3
mark <markpackham1@gmail.com>
2023-11-28 19:56:33 +0000

    show number and string
    https://youtu.be/_u23NOHC6WU?si=sSt5VEh70vbLQBuA&t=219

--- dist/guardsType.js
+++ dist/guardsType.js
@@ -1,6 +1,17 @@
 "use strict";
 // type guards
 function swapIdType2(id) {
-    return id;
+    // Make sure we only deal with strings in this context
+    if (typeof id === 'string') {
+        return parseInt(id);
+        // otherwise deal with numbers
+        // using methods for numbers
+    }
+    else {
+        return id.toString();
+    }
 }
-swapIdType2('5');
+const idTwo = swapIdType2('5');
+const idThree = swapIdType2(100);
+// Now we have an Int and then a Number returned
+console.log(idTwo, idThree);
--- src/guardsType.ts
+++ src/guardsType.ts
@@ -1,11 +1,24 @@
 // type guards
 
+// with type guards we check the type of something before the code is executed
+// this way we don't have runtime errors if we attempt to run
+// a method only intended to work on say strings or only on ints
+
 type IdAgain = number | string;
 
 function swapIdType2(id: IdAgain): IdAgain {
 
-
-    return id;
+    // Make sure we only deal with strings in this context
+    if(typeof id === 'string') {
+        return parseInt(id);
+        // otherwise deal with numbers
+        // using methods for numbers
+    }else{
+        return id.toString();
+    }
 }
 
-swapIdType2('5');
+const idTwo = swapIdType2('5');
+const idThree = swapIdType2(100);
+// Now we have an Int and then a Number returned
+console.log(idTwo, idThree);
