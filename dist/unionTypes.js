01d7e012a3d1c22622264ffca925cbb932792fbd
mark <markpackham1@gmail.com>
2023-11-28 19:14:11 +0000

    demo union type
    https://youtu.be/sgEKZzTCkiY?si=WpuOSpdqQpvLqwxI&t=74

---
+++ dist/unionTypes.js
@@ -0,0 +1,6 @@
+"use strict";
+// union types
+// number or string union type
+let someId;
+someId = 1;
+someId = "1";
---
+++ src/unionTypes.ts
@@ -0,0 +1,7 @@
+// union types
+
+// number or string union type
+let someId: number | string;
+
+someId = 1;
+someId = "1";
